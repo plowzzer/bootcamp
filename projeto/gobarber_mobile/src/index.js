9ba10e628950b56d7aaf41cb0a1ed2dfd0b4c2ec
Pedro Pizzo de Paula <poloo.p3@gmail.com>
2019-12-15 21:42:11 -0300

    start development of app

---
+++ projeto/gobarber_mobile/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './config/reactotronConfig';
+
+import { StatusBar } from 'react-native';
+
+import Routes from './routes';
+// import { Container } from './styles';
+
+export default function App() {
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <Routes />
+    </>
+  );
+}
